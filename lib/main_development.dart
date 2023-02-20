be1ce055167d6f092556dc4243f02bdef91640fd
Wafiq Muhammad Abdul Aziz <wafiqmuhaz@gmail.com>
2023-02-20 13:34:44 +0700

    deploy: firebase

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:my_portfolio_web_app/app/app.dart';
 import 'package:my_portfolio_web_app/bootstrap.dart';
+import 'package:my_portfolio_web_app/firebase_options.dart';
 
-void main() {
-  // ProviderScope(child: App());
-  bootstrap(() => const App());
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+    // ProviderScope(child: App());
+  );
+  await bootstrap(() => const App());
 }
